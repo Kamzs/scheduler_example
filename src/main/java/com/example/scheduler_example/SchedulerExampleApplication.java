e6a5986a5ce9f452322ae8beecab2b7be5e89de8
kamilambroziak <k.ambroziak@samsung.com>
2019-09-16 10:10:46 +0200

    initial

---
+++ src/main/java/com/example/scheduler_example/SchedulerExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.scheduler_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerExampleApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SchedulerExampleApplication.class, args);
+    }
+
+}
