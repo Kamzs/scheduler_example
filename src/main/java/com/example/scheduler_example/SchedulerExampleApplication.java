1d107322c6d1b87ae5622f9f2bae8a02af161086
kamilambroziak <k.ambroziak@samsung.com>
2019-09-16 10:31:36 +0200

    done

--- src/main/java/com/example/scheduler_example/SchedulerExampleApplication.java
+++ src/main/java/com/example/scheduler_example/SchedulerExampleApplication.java
@@ -2,8 +2,10 @@ package com.example.scheduler_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulerExampleApplication
 {
 
